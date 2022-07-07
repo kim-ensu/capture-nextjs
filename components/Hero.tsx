c4e6003bdd07537d668bb3841947e05d8ab74a1e
dkimd5 <dkimd5@mail.ru>
2022-07-07 12:15:33 +0300

    Add elements for Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,17 @@ import React, { FC } from "react";
 type Props = {};
 
 const Hero: FC<Props> = (props) => {
-  return <div>Hero</div>;
+  return (
+    <div>
+      {/* Overlay */}
+      <div />
+      <div>
+        <h1>Heading</h1>
+        <p>Message</p>
+        <button>Book</button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
