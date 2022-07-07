80103b932d2f54becd7c52e1dbe9231a77042e06
dkimd5 <dkimd5@mail.ru>
2022-07-07 13:51:24 +0300

    Add props and props types for Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,19 @@
 import React, { FC } from "react";
 import styles from "styles/Hero.module.css";
 
-type Props = {};
+type Props = {
+  heading: string;
+  message: string;
+};
 
-const Hero: FC<Props> = (props) => {
+const Hero: FC<Props> = ({ heading, message }) => {
   return (
     <div className={styles.container}>
       {/* Overlay */}
       <div className={styles.overlay} />
       <div className={styles.content}>
-        <h1 className={styles.heading}>Heading</h1>
-        <p className={styles.text}>Message</p>
+        <h1 className={styles.heading}>{heading}</h1>
+        <p className={styles.text}>{message}</p>
         <button className={styles.button}>Book</button>
       </div>
     </div>
